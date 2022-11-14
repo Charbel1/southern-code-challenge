931320dd2dc4609fa2775aa8275c15cc7ee4d9af
charbel <charbelnacharn@gmail.com>
2022-11-14 17:16:53 -0400

    property validation

--- core/views/property_view.py
+++ core/views/property_view.py
@@ -4,21 +4,30 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 
 from core.models import Property
+from core.serializers.property_serializers import PropertySerializer
 
 
 class PropertyCreateView(APIView):
 
     def post(self, request, format=None):
-        name = request.data["name"]
-        price = request.data["base_price"]
-        type = request.data["type"]
-        property_obj = Property()
-        property_obj.name = name
-        property_obj.base_price = price
-        property_obj.type = type
-        property_obj.save()
+        # name = request.data["name"]
+        # price = request.data["base_price"]
+        # type = request.data["type"]
+        property_seri = PropertySerializer(data=request.data)
+        if property_seri.is_valid():
+            property_obj= property_seri.save()
+            return HttpResponse(JsonResponse({"property_id": property_obj.id}), content_type="application/json",
+                                status=200)
 
-        return HttpResponse(JsonResponse({"property_id":property_obj.id }),content_type="application/json", status=200)
+
+        return HttpResponse(JsonResponse({"Error": property_seri.errors}), content_type="application/json", status=400)
+
+        # property_obj = Property()
+        # property_obj.name = name
+        # property_obj.base_price = price
+        # property_obj.type = type
+        # property_obj.save()
+
 
 
 class GetModifyDeleteOnePropertyDataView(APIView):
@@ -30,23 +39,32 @@ class GetModifyDeleteOnePropertyDataView(APIView):
 
     def put(self, request,id,format=None):
 
-        name = request.data["name"]
-        price = request.data["base_price"]
-        type = request.data["type"]
-        property_obj = Property.objects.get(id = id)
 
-        property_obj.name = name
-        property_obj.base_price = price
-        property_obj.type = type
-        property_obj.save()
 
-        return HttpResponse(JsonResponse({"property_id": property_obj.id}), content_type="application/json", status=200)
+        property_seri = PropertySerializer(data=request.data)
+        if property_seri.is_valid():
+            try:
+                property_obj = Property.objects.get(id=id)
+            except Property.DoesNotExist:
+                return HttpResponse(JsonResponse({"error":"Property id Error"}), content_type="application/json",
+                                    status=400)
+
+            property_obj.name = request.data["name"]
+            property_obj.base_price=request.data["base_price"]
+            property_obj.type = request.data["type"]
+
+            return HttpResponse(JsonResponse({"success": "data updated"}), content_type="application/json",
+                                status=200)
+
+        return HttpResponse(JsonResponse({"Error": property_seri.errors}), content_type="application/json", status=400)
 
 
     def delete(self, request, id, format=None):
-
-        property_out = Property.objects.get(id=id).delete()
-
+        try:
+            property_out = Property.objects.get(id=id).delete()
+        except Property.DoesNotExist:
+            return HttpResponse(JsonResponse({"error": "property does not exist"}), content_type="application/json",
+                                status=400)
 
         return HttpResponse(JsonResponse({"success": "property delete"}), content_type="application/json", status=200)
 
