09715446083582d92fb21027abb5e8b4ea4b3c2f
charbel <charbelnacharn@gmail.com>
2022-11-15 04:15:03 -0400

    .

--- reservations/settings.py
+++ reservations/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s3jk5!v3f@w+m&)tlvw5+6mdoe0=gg!8+*3q-h8&v@fbs@$rp7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
